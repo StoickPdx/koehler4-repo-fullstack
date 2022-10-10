73b3b6a5d7ce55e10f6421b42293a0d03654729e
Stoic_pdx <bkoehler2019@gmail.com>
2022-10-10 06:57:14 -0700

    Started Reverse finished coins
    Code for reverse is not working, need to work on the

--- hw2/02-reverse.js
+++ hw2/02-reverse.js
@@ -1,3 +1,30 @@
 /** Exercise 02 - Reverse **/
 
-// Add your code here
+// Input of elemends from HTML
+let userInput = document.getElementById("input");
+//Input element for button
+let reverseButton = document.getElementById("reverse");
+//get out put of all work
+let userOutput = document.getElementById("output");
+//use html button to hand when button is pressed
+reverseButton.addEventListener("click", useClick);
+//Function to deal with click
+function useClick(event) {
+  // print out screen that button used
+  console.log("Pressed", event);
+  if (userInput.value.length != 8) {
+    userOutput.style.color = "red";
+    userOutput.style.marginTop = "10px";
+    userOutput.textContent = "Error: Please input an 8-digit number";
+  } else {
+    userOutput.style.color = "green";
+    userOutput.style.marginTop = "10px";
+    userOutput.textContent = userInput.value + " --> " + result;
+  }
+}
+
+const reverseNumber = (input) => {
+  // turn into string, split the number, reverse array, rejoin
+  let reverseInt = parseInt(input.toString().split("").reverse().join(""));
+  return reverseInt;
+};
