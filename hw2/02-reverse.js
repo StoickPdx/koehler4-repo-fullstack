b417c62b2357626db8299aae3ce1691c6415623f
Stoic_pdx <bkoehler2019@gmail.com>
2022-10-13 12:42:57 -0700

    Woked on Reverse and Form

--- hw2/02-reverse.js
+++ hw2/02-reverse.js
@@ -7,19 +7,22 @@ let reverseButton = document.getElementById("reverse");
 //get out put of all work
 let userOutput = document.getElementById("output");
 //use html button to hand when button is pressed
-reverseButton.addEventListener("click", useClick);
+reverseButton.addEventListener("click", handleClick);
 //Function to deal with click
-function useClick(event) {
+function handleClick(event) {
   // print out screen that button used
-  console.log("Pressed", event);
-  if (userInput.value.length != 8) {
-    userOutput.style.color = "red";
-    userOutput.style.marginTop = "10px";
-    userOutput.textContent = "Error: Please input an 8-digit number";
-  } else {
+  console.log("Clicked", event);
+
+  let result = reverseNumber(userInput.value);
+
+  if (userInput.value.length === 8) {
     userOutput.style.color = "green";
-    userOutput.style.marginTop = "10px";
+    userOutput.style.marginTop = "12px";
     userOutput.textContent = userInput.value + " --> " + result;
+  } else {
+    userOutput.style.color = "red";
+    userOutput.style.marginTop = "12px";
+    userOutput.textContent = "Error: Please input an 8-digit number";
   }
 }
 
--- hw2/03-form.js
+++ hw2/03-form.js
@@ -1,3 +1,23 @@
 /** Exercise 03 - Form **/
 
 // Add your code here
+const consoleSubmit = document.getElementById("submit");
+
+consoleSubmit.onclick = function () {
+  //Console group to keep it clean on submit
+  console.group("========= Form Submission =========");
+  console.log("Name: " + document.getElementById("nameInput").value);
+  console.log("Email: " + document.getElementById("emailInput").value);
+  if (!document.getElementById("messageBox").value) {
+    console.log("No feedback was submitted.");
+  } else {
+    console.log("Feedback: " + document.getElementById("messageBox").value);
+  }
+  console.log(
+    "Newsletter: " +
+      (document.getElementById("newsletter").checked
+        ? "Yes, I would like to join the newsletter."
+        : "No, thank you.")
+  );
+  console.groupEnd();
+};
