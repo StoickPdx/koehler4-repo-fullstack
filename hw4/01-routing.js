9202717db76d7db40118019acb0e8c7c316302ba
Stoic_pdx <bkoehler2019@gmail.com>
2022-11-09 15:56:11 -0800

    Finished hw4
    Did not finish Templates as I had a hard time figuring it out.

--- hw4/01-routing.js
+++ hw4/01-routing.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -13,16 +13,16 @@ const port = process.env.PORT || 5000;
 // For other routes, such as http://localhost:5000/other, this exercise should return a status code 404 with '404 - page not found' in html format
 
 const routes = [
-  'welcome',
-  'redirect',
-  'redirected',
-  'cache',
-  'cookie',
-  'other',
+  "welcome",
+  "redirect",
+  "redirected",
+  "cache",
+  "cookie",
+  "other",
 ];
 
 let getRoutes = () => {
-  let result = '';
+  let result = "";
 
   routes.forEach(
     (elem) => (result += `<li><a href="/${elem}">${elem}</a></li>`)
@@ -31,18 +31,57 @@ let getRoutes = () => {
   return result;
 };
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   let routeResults = getRoutes();
 
-  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.writeHead(200, { "Content-Type": "text/html" });
   res.write(`<h1>Exercise 04</h1>`);
   res.write(`<ul> ${routeResults} </ul>`);
   res.end();
 });
 
-app.get('/welcome', (req, res) => {});
-
 // Add your code here
+// Welcome Route
+app.get("/welcome", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h1>Welcome to a new world.</h1>");
+  res.end();
+});
+// Redirect Route
+app.get("/redirect", (req, res) => {
+  res.redirect("/redirected");
+  res.end();
+});
+// Redirected Route
+app.get("/redirected", (req, res) => {
+  res.writeHead(200, { " Content-Type": "text/html" });
+  res.write("<h1>You Have Been Redirected. <h1>");
+  res.end();
+});
+// Redirected Route
+app.get("/cache", (req, res) => {
+  res.writeHead(200, {
+    "Cache-Control": "max-age=86400",
+    "Content-Type": "text/plain",
+  });
+  res.write("This resource was cached");
+  res.end();
+});
+// Cookie Route
+app.get("/cookie", (req, res) => {
+  res.writeHead(200, {
+    "Set-Cookie": "hello=world",
+    "Content-Type": "text/plain",
+  });
+  res.write("cookies... yummm");
+  res.end();
+});
+
+app.get("*", (req, res) => {
+  res.writeHead(404, { "Content-Type": "text/html" });
+  res.write("<h1>404 Not Found</h1>");
+  res.end();
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
--- hw4/02-sessions.js
+++ hw4/02-sessions.js
@@ -1,15 +1,47 @@
-const express = require('express');
-const session = require('express-session');
+const express = require("express");
+const session = require("express-session");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Add your code here
 
 // Use the express-session module
-app.use(/** ... */);
+app.use(
+  session({
+    secret: "12345678",
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 8640000,
+    },
+    resave: false,
+  })
+);
 
-app.get('/', (req, res) => {
+app.use((req, res, next) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.write(`Currently on route: ${req.url}\n \n`);
+
+  console.log(req.session.visitedRoutes);
+  if (req.session.visitedRoutes) {
+    res.write(`Previously Visited:\n`);
+    for (const route of req.session.visitedRoutes) {
+      res.write(`${route}\n`);
+    }
+  } else {
+    req.session.visitedRoutes = [];
+  }
+
+  if (req.url !== "/favicon.ico") {
+    req.session.visitedRoutes.push(req.url);
+  }
+
+  next();
+});
+
+app.get("/", (req, res) => {
   // Add your code here
+  res.write(`Welcome to http://${req.headers.host}`);
+  res.end();
 });
 
 app.listen(port, () => {
